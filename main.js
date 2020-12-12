cd8b27e09fd8e69eea4d34caa9a390d678b6670d
josh choi <wonchoi1994@gmail.com>
2020-12-11 17:02:40 -0800

    user can turn car image

---
+++ main.js
@@ -0,0 +1,17 @@
+var car = document.querySelector("img");
+
+window.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    car.className = "car direction west";
+    car.direction = "west";
+  } else if (event.key === "ArrowRight") {
+    car.className = "car direction east";
+    car.direction = "east";
+  } else if (event.key === "ArrowUp") {
+    car.className = "car direction north";
+    car.direction = "north";
+  } else if (event.key === "ArrowDown") {
+    car.className = "car direction south";
+    car.direction = "south";
+  }
+});
